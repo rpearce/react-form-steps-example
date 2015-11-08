3f977ea43c14949df5afb9336fa7574ec4008814
Robert Pearce <me@robertwpearce.com>
2015-11-08 18:01:21 -0500

    Added basic form info to before area

--- after/src/components/form/Step1.react.js
+++ after/src/components/form/Step1.react.js
@@ -4,26 +4,26 @@ class Step1 extends Component {
   render() {
     const { name, type, age, gender } = this.props.pet;
     return (
-     <div>
-      <div className="stepForm__item">
-        <input type="text" id="stepForm-input1" className="stepForm__input" name="pet[name]" placeholder="Lola" defaultValue={ name } onChange={ this.props.setItem } />
-        <label htmlFor="stepForm-input1" className="stepForm__label">Name</label>
+      <div>
+        <div className="stepForm__item">
+          <input type="text" id="stepForm-input1" className="stepForm__input" name="pet[name]" placeholder="Lola" defaultValue={ name } onChange={ this.props.setItem } />
+          <label htmlFor="stepForm-input1" className="stepForm__label">Name</label>
+        </div>
+        <div className="stepForm__item">
+          <input type="text" id="stepForm-input2" className="stepForm__input" name="pet[type]" placeholder="Golden Retriever" defaultValue={ type } onChange={ this.props.setItem } />
+          <label htmlFor="stepForm-input2" className="stepForm__label">Type</label>
+        </div>
+        <div className="stepForm__item--borderless stepForm__item--nonText">
+          <label className="stepForm__label">Gender</label>
+          <input type="radio" name="pet[gender]" id="stepForm-input4" value="female" checked={ gender === 'female' } onChange={ this.props.setItem } />
+          <label htmlFor="stepForm-input4">Female</label>
+          <input type="radio" name="pet[gender]" id="stepForm-input5" value="male" checked={ gender === 'male' } onChange={ this.props.setItem } />
+          <label htmlFor="stepForm-input5">Male</label>
+        </div>
+        <div className="stepForm__actions">
+          <button className="stepForm__btn--right btn btn--primary" onClick={ this.props.nextStep }>Next Step</button>
+        </div>
       </div>
-      <div className="stepForm__item">
-        <input type="text" id="stepForm-input2" className="stepForm__input" name="pet[type]" placeholder="Golden Retriever" defaultValue={ type } onChange={ this.props.setItem } />
-        <label htmlFor="stepForm-input2" className="stepForm__label">Type</label>
-      </div>
-      <div className="stepForm__item--borderless stepForm__item--nonText">
-        <label className="stepForm__label">Gender</label>
-        <input type="radio" name="pet[gender]" id="stepForm-input4" value="female" checked={ gender === 'female' } onChange={ this.props.setItem } />
-        <label htmlFor="stepForm-input4">Female</label>
-        <input type="radio" name="pet[gender]" id="stepForm-input5" value="male" checked={ gender === 'male' } onChange={ this.props.setItem } />
-        <label htmlFor="stepForm-input5">Male</label>
-      </div>
-      <div className="stepForm__actions">
-        <button className="stepForm__btn--right btn btn--primary" onClick={ this.props.nextStep }>Next Step</button>
-      </div>
-    </div>
     );
   }
 }
