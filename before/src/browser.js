fc2e3699fc81792933f1ee53c26274c037f3e9ff
Robert Pearce <me@robertwpearce.com>
2015-11-10 13:47:39 -0500

    Added boilerplate stuff to get out of way

--- before/src/browser.js
+++ before/src/browser.js
@@ -1,2 +1,10 @@
 /* your client-side bundle code goes here! */
 
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+render(<App />, div);
